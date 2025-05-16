917987290ad71a3f88aa36b83a6fbb6f0f5dff66
pratikmohantyPSJ <pratikmohanty1504@gmail.com>
2025-05-16 12:22:07 +0530

    data ingestion

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import pandas as pd
+from src.logger import logging
+from src.exception import CustomException
+from src.Components.data_ingestion import DataIngestion
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    print(train_data_path,test_data_path)
